01998217c3663029933f1b945ac3c64c402935aa
Gonjs <vanthien7027@gmail.com>
2025-08-21 23:18:30 +0700

    base middleware

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,14 @@
-import express from 'express'
-
+import express, { Request, Response } from 'express'
+import helmet from 'helmet'
+import cors from 'cors'
+import morgan from 'morgan'
+import compression from 'compression'
 const app = express()
 
-app.get('/', (req, res) => {
-  res.send('Hello World')
-})
-
+app.use(helmet())
+app.use(cors())
+app.use(morgan('dev'))
+app.use(compression())
 app.listen(3000, () => {
   console.log('Server is running on port 3000')
 })
